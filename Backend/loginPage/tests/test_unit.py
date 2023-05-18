70de8a7df63947479fbe2d26e404d023805d2f87
@Bluruwu <juandiegotres@hotmail.com>
2023-05-18 13:41:49 -0500

    Avance pruebas unitarias

---
+++ Backend/loginPage/tests/test_unit.py
@@ -0,0 +1,16 @@
+from django.test import TestCase, Client
+
+class TestsCookie(TestCase):
+    def test_csrf(self):
+        client = Client()
+        response = client.get('http://127.0.0.1:8000/api/csrf_cookie')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "CSRF cookie set")
+
+class TestLogin(TestCase):
+    def setUp(self):
+        self.client= Client()
+
+    def test_login_valid(self):
+        response = self.client.post('http://127.0.0.1:8000/api/login')
+        

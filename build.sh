862751d7838d238c40343d3d5b22efbb04d43cac
@Bluruwu <juandiegotres@hotmail.com>
2023-06-07 11:15:24 -0500

    no message

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
+cd ..
 cd Frontend
 npm install --save react-credit-cards --legacy-peer-deps
 npm run build
 cd ..
 cd Backend
 pip install -r requirements.txt
-

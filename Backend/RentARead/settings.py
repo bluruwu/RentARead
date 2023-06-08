3eb38e745b845f46a841cd9973ea431e88332c61
@Bluruwu <juandiegotres@hotmail.com>
2023-06-08 14:33:16 -0500

    dominio corregido

--- Backend/RentARead/settings.py
+++ Backend/RentARead/settings.py
@@ -31,25 +31,25 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    '*.onrender.com'
+    'rentaread-a9yy.onrender.com'
     ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'https://*.onrender.com',
+    'https://rentaread-a9yy.onrender.com',
     
                         ]
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'https://*.onrender.com',
+    'https://rentaread-a9yy.onrender.com',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    'https://*.onrender.com',
+    'https://rentaread-a9yy.onrender.com',
 ]
 
 CORS_ALLOW_CREDENTIALS = True

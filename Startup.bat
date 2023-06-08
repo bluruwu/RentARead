b2299b2b9ca5e3e1c1fd908ddeb25683d0a72759
@Bluruwu <juandiegotres@hotmail.com>
2023-06-08 15:03:16 -0500

    no message

--- Startup.bat
+++ Startup.bat
@@ -3,7 +3,6 @@ CALL cd "%~dp0"Frontend
 CALL npm run build
 CALL cd ..
 CALL cd "%~dp0"backend
-CALL python manage.py collectstatic --no-input
 ::CAMBIAR RUTA A DONDE TIENEN PYTHON INSTALADO, AQUI ->>>>>>>>>>>>>>>>>>
 CALL "%LocalAppData%"\Programs\Python\Python310\python.exe manage.py runserver
 pause
